96245b216b61e2abd6e6ad4a9aa081a64efbb99c
Deon-basson <deonbasson45@gmail.com>
2024-12-07 14:53:24 +0200

    resolved comments

--- src/Components/banner/banner.jsx
+++ src/Components/banner/banner.jsx
@@ -1,21 +1,19 @@
 import React, {useState, useEffect } from "react";
 import './banner.css'
 
-
     const images = [
       "/images/bannerImage.jpg",
       "/images/banner2.png"
     ]
 
-
     function Banner() {
       const [currentIndex, setCurrentIndex] = useState(0);
     
       const moveSlide = (direction) => {
-        if (direction === 'next') {
+        if (direction) {
           setCurrentIndex((prevIndex) => (prevIndex + 1) % images
           .length);
-        } else if (direction === 'prev') {
+        } else {
           setCurrentIndex(
             (prevIndex) => (prevIndex - 1 + images.length) % images.length
           );
@@ -24,28 +22,27 @@ import './banner.css'
     
       useEffect(() => {
         const interval = setInterval(() => {
-          moveSlide('next');
+          moveSlide(true);
         }, 6000); 
     
         return () => clearInterval(interval); 
       }, []);
 
       return (
-        <div className="App">
-          <div className="slider-container">
+        <div className=" flex justify-center items-center mt-2" >
+          <div className="grid slider-container">
             
-            <button className="prev btn-secondary-tgl-sm-active" onClick={() => moveSlide('prev')}>
+            <button className="prev btn-secondary-tgl-sm-active" onClick={() => moveSlide(false)}>
               &#10094;
             </button><div className="slider">
               <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="slider-image"/>
             </div>
-            <button className="next btn-secondary-tgl-sm-active" onClick={() => moveSlide('next')}>
+            <button className="next btn-secondary-tgl-sm-active" onClick={() => moveSlide(true)}>
               &#10095;
             </button>
           </div>
         </div>
       );
     }
-    
 
 export default Banner;
