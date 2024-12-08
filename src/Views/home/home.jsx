10cfebd0416a94b56f60588bfb30a05bced68a52
mdsoftwarelab <mdsoftwarelab@gmail.com>
2024-12-08 10:38:32 +0200

    add game list to home page

--- src/Views/home/home.jsx
+++ src/Views/home/home.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import SidebarLayout from "../../Components/sidebar-layout/sidebar-layout";
 import Promotions from "../../Components/promotions/promotions";
 import Banner from "../../Components/banner/banner";
+import GameList from "../../Components/game-list/GameList";
 
 
 const Home = () => {
     return (
-        <SidebarLayout sidebar={<Promotions />} content={<Banner />}
+        <SidebarLayout sidebar={<Promotions />} content={
+            <>
+                <Banner />
+                <GameList />
+            </>
+        }
       />
     
     )
